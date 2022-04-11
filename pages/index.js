fd3c8b63cc71398b62b71849fe7350b08774096f
Daprojects1 <davidrtm45@gmail.com>
2022-04-11 11:01:00 +0100

    put logic in index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,86 @@
-import { useContext } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import Layout from "../Components/layout"
+import options from '../Utils/apiOptions'
 import NavContext from "../Context/NavContext"
 import InfoNav from '../Components/infoNav'
 import InfoBody from '../Components/infoBody'
 
+
 export default function Home() {
-  const context = useContext(NavContext)
-  let { searching } = context
+  const [searching, setSearching] = useState(false)
+  const [searchValue, setSearchValue] = useState("")
+  const [currentPage, setCurrentPage] = useState("WebSearchAPI")
+  const [searchResults, setSearchResult] = useState("")
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value)
+  }
+  const handleClickSearch = (searchValue) => {
+    if (searchValue.trim().length > 0)
+      grabData(searchValue, currentPage)
+  }
+
+  const handlePressSearch = (e, searchValue) => {
+    if (e.keyCode === 13 && searchValue.trim().length > 0) {
+      grabData(searchValue, currentPage)
+    }
+  }
+
+  const grabData = (value, page) => {
+    setSearching(true)
+    setSearchResult("")
+    axios.request(options(page, value)).then((response) => changePageResults(response.data.value))
+      .catch((error) => {
+        console.error(error);
+        setSearchResult(false)
+      });
+  }
+  const clearSearch = (e) => {
+    document.querySelector("#searchInput").value = ""
+  }
+  const navClick = (stringApi) => {
+    setCurrentPage(stringApi)
+    grabData(searchValue, stringApi)
+  }
+
+  const checkIfActive = (string, obj) => {
+    if (string === currentPage) return obj
+    else return {}
+
+  }
+  const changePageResults = (data) => {
+    setSearchResult(data)
+  }
+  const sendToHome = () => {
+    document.querySelector("#searchInput").value = ""
+    setSearchResult("")
+    setSearchValue("")
+    setSearching(false)
+  }
+  const contextData = {
+    searching,
+    searchValue,
+    handleSearch,
+    handleClickSearch,
+    handlePressSearch,
+    clearSearch,
+    navClick,
+    checkIfActive,
+    searchResults,
+    currentPage,
+    sendToHome,
+  }
+
   return (
     <div>
-      {searching ? <InfoNav /> : ""}
-      {searching ? <InfoBody /> : ""}
+      <NavContext.Provider value={contextData}>
+        <Layout >
+          {searching && <>
+            <InfoNav />
+            <InfoBody />
+          </>}
+        </Layout>
+      </NavContext.Provider>
     </div>
   )
 }

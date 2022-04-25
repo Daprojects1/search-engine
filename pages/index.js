0999a012486ab090ee887496c5f782c0ac60677d
Daprojects1 <davidrtm45@gmail.com>
2022-04-25 09:41:01 +0100

    fix web bug

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useState } from 'react'
 import axios from 'axios'
 import Layout from "../Components/layout"
 import options from '../Utils/apiOptions'
